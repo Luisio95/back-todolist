9bd62c1eac69596951e986305038cd3cda13c56b
Luis Rivera <riveragranadosl@gmail.com>
2025-06-27 22:18:33 -0600

    add

--- backend/dist/database.js
+++ backend/dist/database.js
@@ -9,6 +9,6 @@ dotenv_1.default.config();
 const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: 59708,
+    port: 3306,
 });
 exports.default = sequelize;
