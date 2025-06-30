7a493b44ac9205a0ce654ff5b81d205fac16732e
Luisio95 <115296164+Luisio95@users.noreply.github.com>
2025-06-30 14:20:05 -0600

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,9 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  allowedHeaders: ["Content-Type"]
 }));
 
 // Swagger
